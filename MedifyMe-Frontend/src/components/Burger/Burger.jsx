68b1c3aa78cf37af1171462b09580f1a00a4aaa0
ayush-beniwal <103768983+ayush-beniwal@users.noreply.github.com>
2023-04-08 02:22:36 +0530

    fixed navbar and footer

--- MedifyMe-Frontend/src/components/Burger/Burger.jsx
+++ MedifyMe-Frontend/src/components/Burger/Burger.jsx
@@ -17,10 +17,9 @@ const Burger = (patient) => {
         <li><Link to="/health_history">Health History</Link></li>
         <li><Link to="/prescription">Prescriptions</Link></li>
         <li><Link to="/test">Tests & Reports</Link></li>
-        <li><Link style={{ color: "black" }} to="/appointment">Appointment</Link></li>
+        <li><Link to="/appointment">Appointment</Link></li>
         <li>
             <div className={styles.signIn}>
-                <img alt="account" src={Account} />
                 {!patient.isLoggedIn ? (
                   <Link to="/login">Sign In</Link>
                 ) : (
