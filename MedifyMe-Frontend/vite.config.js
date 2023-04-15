08877c9ffb1363eeab037d8766fa9784eb34de04
SaiRev <saiyam.jain.mec21@itbhu.ac.in>
2023-04-16 02:26:10 +0530

    Lazy Loading

--- MedifyMe-Frontend/vite.config.js
+++ MedifyMe-Frontend/vite.config.js
@@ -1,21 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import compression from "vite-plugin-compression";
 import dotenv from "dotenv";
-// import httpProxy from "http-proxy";
 dotenv.config();
 
-// const SERVER_URL = import.meta.env.VITE_SERVER_URL;
-
-// function createMyProxy() {
-//   return httpProxy.createProxy({
-//     target: process.env.VITE_SERVER_URL,
-//     changeOrigin: true,
-//     pathRewrite: { "^/server": "" },
-//   });
-// }
-
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: "gzip",
+      ext: ".gz",
+      deleteOriginalAssets: false,
+    }),
+  ],
   server: {
     port: 80,
     host: "0.0.0.0",
